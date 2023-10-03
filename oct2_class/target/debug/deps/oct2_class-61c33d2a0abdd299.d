e4edd64558f05a94d03363ef3524d859061f584d
GIR7 <0218tyh@gmail.com>
2023-10-03 10:57:12 -0700

    rm target dir

--- oct2_class/target/debug/deps/oct2_class-61c33d2a0abdd299.d
+++
@@ -1,5 +0,0 @@
-C:\git_vscode\Rust-class\oct2_class\target\debug\deps\oct2_class-61c33d2a0abdd299.rmeta: src\main.rs
-
-C:\git_vscode\Rust-class\oct2_class\target\debug\deps\oct2_class-61c33d2a0abdd299.d: src\main.rs
-
-src\main.rs:
--- oct2_class/target/debug/deps/oct2_class-b1b99d72142d5079.d
+++
@@ -1,5 +0,0 @@
-C:\git_vscode\Rust-class\oct2_class\target\debug\deps\oct2_class-b1b99d72142d5079.rmeta: src\main.rs
-
-C:\git_vscode\Rust-class\oct2_class\target\debug\deps\oct2_class-b1b99d72142d5079.d: src\main.rs
-
-src\main.rs:
