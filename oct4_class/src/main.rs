ca996db78a6efae5ac5a2f37a276d69848209af8
GIR7 <0218tyh@gmail.com>
2023-10-04 17:08:04 -0700

    struct

--- oct4_class/src/main.rs
+++ oct4_class/src/main.rs
@@ -1,20 +1,21 @@
+
+
+fn sum(v: Vec<u64>) -> Point{
+    let mut total = 0;
+    for x in v{
+        total += x;
+    }
+    Point{x:total, y:total}
+}
+
+struct Point{
+    x: u64,
+    y: u64,
+}
 fn main() {
    let mut v = Vec::new();
    v.resize(10000,0);
    v[5] = 125;
    let s = sum(v);
-   println!("{s}");
-}
-
-fn sum(v: Vec<u64>) -> u64{
-    let mut total = 0;
-    for x in v{
-        total += x;
-    }
-    total
-}
-
-struct Point{
-    x: u64,
-    y:u64,
+   println!("{}",s.x+s.y);
 }
