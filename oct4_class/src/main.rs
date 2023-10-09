5ed65a4264a0a830779a39e0ecb21b524f51a646
GIR7 <0218tyh@gmail.com>
2023-10-08 21:26:47 -0700

    arrays

--- oct4_class/src/main.rs
+++ oct4_class/src/main.rs
@@ -1,18 +1,45 @@
+//strings and aggerate
+
+
+
 fn main() {
-   let mut v = Vec::new();
-   v.resize(10000,0);
-   v[5] = 125;
-   let s = sum(v);
-   println!("{}",s.x+s.y);
+//    let mut v = Vec::new();
+//    v.resize(10000,0);
+//    v[5] = 125;
+//    let s = sum(v);
+//    println!("{}",s.x+s.y);
+
+    let mut arr = [0;1_000];//mem is living on the stack
+    arr[5] = 77; //crates a copy
+    let res = sum(arr);
+    
+    println!("Sum is {}", res);
+    
+    arr[7] = 55; 
+    let res = sum(arr);
+    
+    println!("Sum is {}", res);
 }
 
-fn sum(v: Vec<u64>) -> Point{
+fn sum(v: [u64;1_000]) -> u64{
     let mut total = 0;
+  //  for i in 0..v.len() {//the loop variable `i` is only used to index `v`
+        //onsider using an iterator
+    //     for <item> in &v 
     for x in v{
         total += x;
     }
-    Point{x:total, y:total}
+    total 
 }
+
+
+// fn sum(v: Vec<u64>) -> Point{
+//     let mut total = 0;
+//     for x in v{
+//         total += x;
+//     }
+//     Point{x:total, y:total}
+// }
 
 struct Point{
     x: u64,
