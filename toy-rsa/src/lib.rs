1ca7dfb6c400a2d6319c6caefdf8885f0ac2bc80
GIR7 <0218tyh@gmail.com>
2023-10-11 21:44:05 -0700

    genkey()

---
+++ toy-rsa/src/lib.rs
@@ -0,0 +1,58 @@
+use toy_rsa_lib::* ;
+
+/// Fixed RSA encryption exponent.
+pub const EXP: u64 = 65_537;
+
+
+/// Generate a pair of primes in the range `2**31..2**32`
+/// suitable for RSA encryption with exponent.
+pub fn genkey() -> (u32, u32){
+    loop{
+        let p = toy_rsa_lib::rsa_prime();//returns a u32
+        let q = toy_rsa_lib::rsa_prime();
+        let p = u64::from(p);//converts to u64 for lcm function
+        let q = u64::from(q);
+        let lambda = toy_rsa_lib::lcm(p-1,q-1);
+        if EXP < lambda && toy_rsa_lib::gcd(EXP,lambda)==1{
+            let p: u32 = u32::try_from(p).unwrap();
+            let q: u32 = u32::try_from(q).unwrap();
+            return (p,q);// function returns a (u32,u32)
+        }
+    }
+}
+
+
+// /// Encrypt the plaintext `msg` using the RSA public `key`
+// /// and return the ciphertext.
+// pub fn encrypt(key: u64, msg: u32) -> u64{
+
+// }
+
+
+// /// Decrypt the cipertext `msg` using the RSA private `key`
+// /// and return the resulting plaintext.
+// pub fn decrypt(key: (u32, u32), msg: u64) {
+    
+// }
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_help_function() {
+        let p: u64 = 7;
+        let q: u64 = 11;
+        // Expected result
+        let expected_result: u64 = 30;  // LCM of (7 - 1) and (11 - 1) = LCM(6, 10) = 30
+
+        // Call the help function
+        let result = helper(p, q);
+
+        // Assert that the result matches the expected value
+        assert_eq!(result, expected_result);
+    }
+}
