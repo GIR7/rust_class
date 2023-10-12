635c4343f9c9393cba80f7e04bb3fdf2d785a697
GIR7 <0218tyh@gmail.com>
2023-10-11 18:03:15 -0700

    enum

--- oct11_class/src/main.rs
+++ oct11_class/src/main.rs
@@ -3,12 +3,40 @@
 /* reference counting
 */
 use std::cell::RefCell;
-
+ struct Twostring {
+        s1: String,
+        s2: String,
+    }
 
 fn main() {
+    //enum
+    /* 
+    pub enum Option<T> {
+          None,
+          Some(T),
+      }
+      pub use Option::*;
+
+      // let x: &u8 = 0; // Compiler says no
+      let x: Option<&u8> = None;
+      // let Some(ptr) = x; // Compiler says no
+      match x {
+          None => {
+              Err("missing x")
+          }
+          Some(&x) if x == 7 => {
+              println!("all good");
+              Ok(())
+          }
+          Some(&x) => {
+              Err("bad x")
+          }
+        }
+        */
+
 
    // Moving a field out of a struct makes it "partially moved"
-    let t = TwoString {
+    let t = Twostring {
         s1: "hello".to_string(),
         s2: "world".to_string(),
     };
@@ -19,7 +47,6 @@ fn main() {
     // return t; // compiler error: t partially moved
 
 
-    
     //RefCell keeps track of the all the borrowd ref at run time
     let r = RefCell::new(0u8);
     // let r1 = r.borrow(); // returns a guard type called 'Ref'
