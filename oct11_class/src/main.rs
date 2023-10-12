199e315a5e848f32ff0e73ed2e564201028d08ab
GIR7 <0218tyh@gmail.com>
2023-10-11 17:06:57 -0700

    refcell???...

--- oct11_class/src/main.rs
+++ oct11_class/src/main.rs
@@ -1,3 +1,15 @@
+//topic: struct, enum, pattern match, rust genaric 
+
+/* reference counting
+*/
+use std::cell::RefCell;
+
 fn main() {
-    println!("Hello, world!");
+    //RefCell keeps track of the all the borrowd ref at run time
+    let r = RefCell::new(0u8);
+    // let r1 = r.borrow(); // returns a guard type called 'Ref'
+    let  mut rml = r.borrow_mut();
+    *rml += 1;
+    // drop(rml);
+    println!("refcell: {}", rml);
 }
