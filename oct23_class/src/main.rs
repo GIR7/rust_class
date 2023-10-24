df4638d73619b9a5437bb482763283482bb4a3f6
GIR7 <0218tyh@gmail.com>
2023-10-24 11:10:55 -0700

    .

---
+++ oct23_class/src/main.rs
@@ -0,0 +1,11 @@
+//review - trait:
+/*Orphan rules
+If you are implementing the trait/type, trait/type has to be defined in your own crate
+ */
+
+/*subtraits:   */
+
+//Clone: Does a deep copy: copy the whole thing in a new mem, shallow copy: copy only ref to points same mem
+fn main() {
+
+}
