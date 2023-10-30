c3875b1caf857a679299865d2f307b96314e26fb
GIR7 <0218tyh@gmail.com>
2023-10-29 23:09:15 -0700

    more notes

--- oct16_class/src/main.rs
+++ oct16_class/src/main.rs
@@ -65,4 +65,130 @@ Crates typically follow the principles of Semantic Versioning (SemVer),
  where the major version may change for breaking changes, 
  minor versions for backwards-compatible additions, 
  and patch versions for backwards-compatible bug fixes. 
+
+ for Rust: 
+ Three-part version numbers: breaking.major.minor
+Rust convention also says 0.breaking.minor
+
+ Cargo.toml has version matching rules: usually want just the breaking part
+Cargo.lock preserves Cargo's versioning choices
  */
+
+ /* By defualt, 
+ src/main.rs - binary root
+ src/lib.rs - library crate root
+ src/mod.rs - module top-level for a library
+
+src/bin/prog.rs is a named binary prog
+src/bin/prog/main.rs is a named binary prog with modules
+examples/ is a bin directory usually used for example of library crates usage. - 'cargo run --example foo' runs the example named 'foo' from here
+tests/ contains extra tests, usually used with library crates
+benches/ contains benchmarks [unstable-ish]. Usauslly use 'criterion-rs'
+  
+  */
+
+  /*Namespaces are :: separated as in some other languages
+std::io::stdin()
+use std::io::stdin; then stdin();
+use std::io::{self, stdin};, then io::stdin() or stdin() 
+*/
+
+/*Dependencies in the toml file:
+Instead of
+
+  [dependencies]
+  foo = { git = "http://github.com/something/foo.git" }
+Try
+
+  [dependencies.foo]
+  git = "http://github.com/something/foo.git" 
+  */
+
+/*'pub'
+By default, nothing in a module is visible outside the module/crate in which it is declared
+
+You can put pub in front of an item to make it externally visible
+
+For things like structs and enums, the visibility of the thing and its fields is controlled separately
+pub struct foo{f1:int } is different pub struct foo{ pub f1:int } 
+
+The compiler will whine at you about an unused thing unless it is pub, in which case it assumes you just know it will be used somewhere
+ */
+
+ /*Docs - rustdoc 
+Doc comments have an extra slash ///
+
+Inline doc comments are //!
+You can also have C-style doc comments /** or /*!
+Use cargo doc --document-private-items to get docs for stuff not visible outside the crate
+
+Doc comments are treated as Markdown
+*/
+
+*/Tests
+Marking a function with #[test] makes it a test
+Tests fail by panicking (#[should_panic] tests fail by not panicking)
+Test panics usually from asserts: assert!(), assert_eq!()
+Marking an item #[cfg(test)] makes it included only when compiling for testing
+Examples in doc comments are treated as "doc tests" by default: must compile and not panic ---- !
+  */
+
+//Traits:
+/* 
+pub trait Vegetable{
+  fn desc(&self) -> &'static str;
+}
+
+pub struct Lettuce;
+impl Vegetable for Lettuce{
+  fn desc(&self) -> &'static str{
+    self.desc()
+  }
+}
+impl Lettuce{
+  fn desc(&self) -> &'static str{
+    "Lettuce"
+  }
+}
+
+pub struct Tomato;
+impl Vegetable for Tomato{
+  fn desc(&self) -> &'static str{
+    self.desc()
+  }
+}
+impl Tomato{
+  fn desc(&self) -> &'static str{
+    "Tomato"
+  }
+}
+
+pub struct GenericSalad<T: Vegetable>{
+  veggies:Vec<T>, // a 'GenericSalad' is the vector of 'Vegetable' - lettuce, tomato
+}
+impl <T:Vegetable>GenericSalad<T>{
+  fn new() -> Self{
+    GenericSalad { veggies: Vec::new() }
+  }
+  //adds a new 'GenericSalad'
+  fn add(&mut self,v:T){
+    self.veggies.push(v); // push a veggies to 'GenericSalad'
+  }
+  //display a 'GenericSalad'
+  fn display(&self){
+    for v in &self.veggies{
+      println!("{}",v.desc()); // display a 'GenericSalad' BY display all the vegetables are in it
+    }
+  }
+}
+
+//dynamic traits:
+pub struct TraitSalad{
+  veggies: Vec<Box<dyn Vegetable>> // we don't decide it at compile time
+}
+impl TraitSalad {
+  fn new() -> Self{
+    TraitSalad { veggies: Vec::new() }
+  }
+}
+*/
