5d154be53faecbd30d4a190be1f2c855364ee0f8
GIR7 <0218tyh@gmail.com>
2023-10-30 17:45:07 -0700

    closure

--- oct30_class/src/main.rs
+++ oct30_class/src/main.rs
@@ -7,9 +7,16 @@ References are available (indeed, a function essentially is a static reference)
  */
 
  /*Closure
-
-
+The problem with pure functions is that anything they compute has to be based on arguments
+A closure is a function that is allowed to access and mutate its "environment": names that are statically in scope at the point of its declaration
+This means that different calls to the function with the same parameters may produce different results
+Closures in Rust are mostly first-class, and can access parameters and locals like anything else
+Syntax has wacky pipes and stuff; types are optional and inferred
   */
+
+/*
+
+ */
 fn main() {
    let y = "hello".to_string();
 
