6020223ba5e35d83b5157a247cd3f5077810be7c
GIR7 <0218tyh@gmail.com>
2023-11-01 21:26:16 -0700

    user input creates board

--- chomp-game/src/main.rs
+++ chomp-game/src/main.rs
@@ -1,6 +1,12 @@
-use chomp_game::Board
+use chomp_game::Board;
+use prompted::input;
 
 fn main() {
+    ///input! macro returns a String, 
+    ///convert it to usize.
+    let width:usize = input!("input a numer size width: ").parse::<usize>().unwrap();
+    let height:usize = input!("input a numer size width: ").parse::<usize>().unwrap();
+    let mut game_board = Board::new(width,height);
+
     
-    let mut game_board = new(width,height);
 }
