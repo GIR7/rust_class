02fe32cff8edc9889780b4b40aae080c7f13650b
GIR7 <0218tyh@gmail.com>
2023-11-06 20:32:23 -0800

    print function

--- chomp-game/src/lib.rs
+++ chomp-game/src/lib.rs
@@ -24,10 +24,13 @@ impl Board{
     ///prints the board 
     //- Ref: chatgpt
     pub fn print(&self) {
-        let max_x = self.state.iter().map(|&(x, _)| x).max().unwrap_or(0);
+        //calculates the maximum x coordinate in the HashSet:state
+        let max_x = self.state.iter().map(|&(x, _)| x).max().unwrap_or(0);//if empty then return 0
+        //then gets the maximum y in the hashset
         let max_y = self.state.iter().map(|&(_, y)| y).max().unwrap_or(0);
 
-        for y in (0..=max_y).rev() {
+        //loop throught the hashset and print out the state
+        for y in (0..=max_y).rev() {//print it from the top of the grid
             for x in 0..=max_x {
                 if self.state.contains(&(x, y)) {
                     print!("O ");
@@ -38,4 +41,9 @@ impl Board{
             println!();
         }
     }
+
+    /// This function checks the board status and to determine if the player or AI already won or lose. 
+    /// This function will be called after every move made by AI or the player
+    /// If the status of the game is already won or lose, then it will return the results of the game 
+    /// 
 }
